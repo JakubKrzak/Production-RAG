76a9d53d1d8e28c5cb356ad9fd479970679bb6d5
JakubKrzak <jakub.krzak.main@gmail.com>
2026-02-19 09:14:07 +0100

    Add 2 schemas for request

--- src/rag/services/schemas.py
+++ src/rag/services/schemas.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
 from typing import Any
 
+#Schemas for creating documents and chunks
 class DocumentChunk(BaseModel):
     chunk_id: str
     text: str
     metadata: dict[str, Any]
+
+#Schemas for receiving question and answer
+class QuestionReq(BaseModel):
+    question: str
+
+class QuestionRes(BaseModel):
+    answer: str
