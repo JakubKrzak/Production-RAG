c876c261daf575e590c37d5baf7828d2f0f33f88
JakubKrzak <jakub.krzak.main@gmail.com>
2026-02-19 10:37:23 +0100

    add check_collection_exists function to pipeline

--- src/rag/services/ingestion.py
+++ src/rag/services/ingestion.py
@@ -1,7 +1,6 @@
-from loaders.data_loader import load_chunk_pdf
-from services.llm_service import LLM_server
-from storage.data_base import QdrantStorage
+from rag.loaders.data_loader import load_chunk_pdf
 from dotenv import load_dotenv
+from qdrant_client import QdrantClient
 
 load_dotenv()
 
@@ -11,8 +10,14 @@ class Ingestion():
         self.llm = llm
     
     def ingestion_pdf(self, pdf_path):
+        collection_exists = self.db.check_collection_exists()
+        if not collection_exists:
+            self.db.create_collection()
+        
         chunks_pdf = load_chunk_pdf(path=pdf_path)
+        
         chunks_embedding = self.llm.embeddings_chunks(chunks_pdf)
+        
         if self.db.upsert_chunks(chunks=chunks_pdf, vectors=chunks_embedding):
             return f"PDF {pdf_path} has been ingested"
 
