714abd377fcd3d11a5584f46c6d80b1d0564c84c
JakubKrzak <jakub.krzak.main@gmail.com>
2026-02-19 08:56:02 +0100

    delete mistake in variable name

--- src/rag/services/retrival.py
+++ src/rag/services/retrival.py
@@ -1,9 +1,9 @@
-from llm_service import LLM_server
-from data_base import QdrantStorage
+from rag.services.llm_service import LLM_server
+from rag.storage.data_base import QdrantStorage
 from dotenv import load_dotenv
 
 class Retrival():
-    def __init__(self, data_base: str, llm: str):
+    def __init__(self, data_base, llm):
         self.db = data_base
         self.llm = llm
     
@@ -12,4 +12,4 @@ class Retrival():
         hits = self.db.search_similarity(vectors = user_question_embedding, top_k=5)
         context = self.llm.create_context(hits=hits)
         answer = self.llm.llm_response(user_question=user_question, context=context)
-        print(answer)
+        return answer
