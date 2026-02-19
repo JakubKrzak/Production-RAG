87064971d28c2ac282ae75b3227e874c6cc696e5
JakubKrzak <jakub.krzak.main@gmail.com>
2026-02-19 08:38:52 +0100

    Create enpoint to collect user questions

--- src/rag/main.py
+++ src/rag/main.py
@@ -1,24 +1,43 @@
-from src.loaders.data_loader import load_chunk_pdf
-from src.services.llm_service import LLM_server
-from src.storage.data_base import QdrantStorage
-from src.services.retrival import  Retrival
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from dotenv import load_dotenv
 
-def main():
-    load_dotenv()
-    db = QdrantStorage("RAG-DOC")
-    ai = LLM_server()
-    retrival = Retrival(db, ai)
-    
-    print("RAG system based on PDF")
-    
-    while True:
-        user_question = input("Ask your question: ")
-        if user_question.lower() in ["exit"]:
-            break
-        anserw = retrival.retrival(user_question)
-        print(anserw)
+from rag.services.llm_service import LLM_server
+from rag.storage.data_base import QdrantStorage
+from rag.services.retrival import Retrival
 
-    
-if __name__ == "__main__":
-    main()
+
+load_dotenv()
+db = QdrantStorage("RAG-DOC")
+ai = LLM_server()
+pipeline = Retrival(db, ai)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500", "http://localhost:5500",
+        "http://127.0.0.1:5501", "http://localhost:5501",
+        "http://127.0.0.1:5502", "http://localhost:5502",
+    ],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"messages": "API works"}
+
+class QuestionReq(BaseModel):
+    question: str
+
+class QuestionRes(BaseModel):
+    answer: str
+
+@app.post("/question", response_model=QuestionRes)
+def ask_question(payload: QuestionReq):
+    answer = pipeline.retrival(payload.question)
+    return {"answer": answer}
