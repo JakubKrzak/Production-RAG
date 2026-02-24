c6458acb0a068753f377368a80a922209b917ee5
JakubKrzak <jakub.krzak.main@gmail.com>
2026-02-24 11:10:39 +0100

    Delete import

--- src/rag/loaders/data_loader.py
+++ src/rag/loaders/data_loader.py
@@ -2,7 +2,6 @@ from llama_index.readers.file import PDFReader
 from llama_index.core.node_parser import SentenceSplitter
 from rag.services.schemas import DocumentChunk
 from pathlib import Path
-from dotenv import load_dotenv
 
 def load_chunk_pdf(path: str) -> list[DocumentChunk]:
     """
